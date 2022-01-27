d074a88be058d2e76ec63c696af7f436ffecbc41
giulio30795 <giulioincarbone95@gmail.com>
2022-01-27 13:46:28 +0100

    add buttons

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
     <div class="container my-5">
+        <button class="btn btn-warning">
+            <a class="text-decoration-none text-light" href="{{route('comics.create')}}">Aggiungi un fumetto all'archivio</a>
+        </button>
         <table class="table">
-            <tr>
+            <tr class="text-center">
                 <th>ID</th>
                 <th>Name</th>
                 <th>Price</th>
                 <th>Series</th>
+                <th>Actions</th>
             </tr>
     
             @foreach ($comics as $comic )
@@ -17,15 +21,18 @@
                     <td> € {{ $comic->price }}</td>
                     <td>{{ $comic->series }}</td>
                     <td>
-                        <button>
-                            <a href="{{ route('comics.show', $comic->id) }}">Vedi Dettagli</a>
+                        <button class="btn  btn-success">
+                            <a class="text-decoration-none text-light" href="{{ route('comics.show', $comic->id) }}">Vedi Dettagli</a>
+                        </button>
+                        <button class="btn  btn-primary mx-2">
+                            <a class="text-decoration-none text-light" href="{{ route('comics.show', $comic->id) }}">Modifica</a>
+                        </button>
+                        <button class="btn  btn-danger">
+                            <a class="text-decoration-none text-light" href="{{ route('comics.show', $comic->id) }}">Elimina</a>
                         </button>
                     </td>
                 </tr>
             @endforeach
         </table>
-        <button>
-            <a href="{{route('comics.create')}}">Aggiungi un fumetto all'archivio</a>
-        </button>
     </div>
 @endsection
